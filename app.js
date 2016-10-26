7a3cec3ecc816177ba7e1e1126bd3e904135400d
patriciaa <patriciatorvalds@gmail.com>
2016-10-25 20:45:16 -0400

    Added README, finished app.js

--- app.js
+++ app.js
@@ -4,15 +4,47 @@ firebase.initializeApp({
   databaseURL: "https://echo-pmt.firebaseio.com"
 });
 
+var stripe = require("stripe")(
+ ""
+);
+
 var db = firebase.database();
 var ref = db.ref("/");
 
 ref.on("child_added", function(snapshot) {
   console.log(snapshot.key, snapshot.val());
-  // TODO: call Stripe API
-  // three nested: get updates
-  // post to stripe
-  // when stripe finishes, save back to firebase
+  var orderRef = ref.child(snapshot.key);
+  var order = snapshot.val()
+
+  stripe.customers.create({
+    source: order.payment
+  })
+    .then(function(customer) {
+      // Save customer id to Firebase
+      var customerRef = orderRef.child("customer");
+      return customerRef.update({
+        stripeId: customer.id
+      })
+        .then(function() {
+          return customer;
+        });
+    })
+    .then(function(customer) {
+      console.log("customer id saved")
+      // Charge customer
+      return stripe.charges.create({
+        amount: order.product.price*100, // Amount in cents
+        currency: "usd",
+        customer: customer.id
+      });
+    })
+    .then(function(charge) {
+      // Save charge to Firebase -- similar to product, using orderRef (nothing overwritten)
+      var chargeRef = orderRef.child("charge");
+      return chargeRef.update({
+        charge: charge.id
+      })
+    });
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code)
 });
